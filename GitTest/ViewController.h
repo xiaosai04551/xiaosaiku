cb4d55c59b6cefd232aec242d480b06a91fad9ef
xiaosai <xiaosai@xiaosaidemacbook-air.local>
2018-03-09 09:34:01 +0800

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by xiaosai on 2018/3/9.
+//  Copyright © 2018年 xiaosai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by xiaosai on 2018/3/9.
+//  Copyright © 2018年 xiaosai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
